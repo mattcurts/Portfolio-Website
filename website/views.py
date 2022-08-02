e61e78844e6e00884549e43e2f457a8dbcdd3bcb
Matt <63571383+mattcurts@users.noreply.github.com>
2022-08-02 00:31:04 +0000

    Fixed up Home Page and Random Art

--- website/views.py
+++ website/views.py
@@ -1,22 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_file
+from .Art import Svg, ArtConfig
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", html_file='/Art/embeddedArt.html')
 
 
 @views.route('/art')
 def art():
-    # Art.main()
-    #return render_template('a432.html')
-    return render_template("art.html")
+    svg = Svg(ArtConfig(count=2000),
+              "./website/templates/Art/GeneratedArtSVG.html")
+    svg.make_svg()
+
+    return render_template("randomArt.html",
+                           html_file='/Art/GeneratedArtSVG.html')
 
 
 @views.route('/resume')
 def resume():
-    # Art.main()
     #return render_template('a432.html')
-    return render_template("index.html")
+    return send_file('static/MatthewCurtisResume.pdf', as_attachment=True)
