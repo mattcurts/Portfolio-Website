eabbbcda4612532dc1b53e64993eba72b7e64d5d
Matt <63571383+mattcurts@users.noreply.github.com>
2022-10-15 20:25:21 +0000

    Updated Resume

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from website import create_app
+from flask import Flask
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
--- replit.nix
+++ replit.nix
@@ -1,19 +1,7 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs-16_x
-    pkgs.python38Full
+    pkgs.python39Packages.pip
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
-    ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-    LANG = "en_US.UTF-8";
-  };
 }
